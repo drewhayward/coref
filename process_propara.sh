8e1fdc58592ef158c46679b1c65073771c6fddbe
Drew Hayward <d.andrew.hayward@gmail.com>
2021-05-14 12:29:20 -0400

    update script with argument path reference

--- process_propara.sh
+++ process_propara.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-python propara_to_jsonlines.py /data/hlr/updated_propara_data/test_propara.json test_propara_formatted.jsonlines data/spanbert_large/vocab.txt
-python propara_to_jsonlines.py /data/hlr/updated_propara_data/train_propara.json train_propara_formatted.jsonlines data/spanbert_large/vocab.txt
-python propara_to_jsonlines.py /data/hlr/updated_propara_data/dev_propara.json dev_propara_formatted.jsonlines data/spanbert_large/vocab.txt
+python propara_to_jsonlines.py $1/test_propara.json test_propara_formatted.jsonlines data/spanbert_large/vocab.txt
+python propara_to_jsonlines.py $1/train_propara.json train_propara_formatted.jsonlines data/spanbert_large/vocab.txt
+python propara_to_jsonlines.py $1/dev_propara.json dev_propara_formatted.jsonlines data/spanbert_large/vocab.txt
